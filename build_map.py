f2c6e7e11ddda7e120e99ee511be134f4fe9c786
infery <deulinartem@gmail.com>
2019-09-26 13:14:46 +0000

    Python3 and a bit of translation

--- build_map.py
+++ build_map.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
+'''
+Create network map in zabbix or just check transit for one ip
+'''
+
 
 from itertools import groupby
 from operator import itemgetter
@@ -32,15 +36,14 @@ if 'use_zabbix' in cfg['zabbix']:
         use_zabbix = False 
     else:
         use_zabbix = True
-        print 'Cant understend option use_zabbix, default is True'
+        print("Can't understend option use_zabbix, default is True")
 
-
-# для начала попытаемся найти все "крайние свичи", которые не являются транзитными
-# это те свичи, у которых маки железок или на аплинке или нет в fdb
+# For begining we will try to find all nontransit switches (edge switch)
+# Nontransit is that switch, which has MACs only on uplink port
 edge_devices = []
 devices = {}
 debug = True 
-edge_with_transits = {} # словарь edge с отсортированным списком транзитов
+edge_with_transits = {} # dict of edge with list of transit switches
 
 
 def progress(i, msg):
@@ -50,7 +53,7 @@ def progress(i, msg):
 
 
 def get_devices_list_from_db():
-    '''ДОстаем всю информацию о железках из базы для последующего быстрого доступа'''
+    '''Get all info form database'''
     with sqlite3.connect(dbname) as con:
         con.row_factory = sqlite3.Row
         result = con.execute("""SELECT arp.ip AS ip,
@@ -71,7 +74,7 @@ def get_devices_list_from_db():
 
 
 def calc_uplink_mac():
-    '''Фунция возвращает самый "популярный" мак-адрес в mac_address_table.'''
+    '''Returns most popular mac address. It is uplink mac usually'''
     macs = []
     with sqlite3.connect(dbname) as con:
         con.row_factory = sqlite3.Row
@@ -90,8 +93,7 @@ def calc_uplink_mac():
 
 
 def get_set_uplinks():
-    '''Функиция заполняет поле аплинка в словаре свича и проставляет поле uplink в базе.
-    Это поле потом понадобится, чтобы найти транзитные свичи и отсортировтаь их'''
+    '''Set uplink port for switch in the database and in the dict'''
     with sqlite3.connect(dbname) as con:
         for i, device in enumerate(devices.keys()):
             uplink = get_port_by_mac(ip=device, mac=uplink_mac)
@@ -107,10 +109,9 @@ def get_set_uplinks():
 
 
 def switch_is_edge(switch_ip, uplink_port):
-    '''для всех маков в таблице ARP проверяем на этом свиче, есть ли мак
-       на каком-нибудь порту, отличном от аплинка. Если нет, значит железка крайняя.
-       На вход принимает IP свича, который нужно проверить является ли он крайним.
-       Если свич является edge - заносим эту информацию в базу, она нам пригодится'''
+    '''show mac address-table address (`all arp table`) and port != uplink
+    If we will find any mac on port, other than uplink, this switch is not edge.
+    Otherwise switch is edge. Save this info to the database'''
     edge = True
     with sqlite3.connect(dbname) as con:
         cursor = con.cursor()
@@ -134,9 +135,7 @@ def switch_is_edge(switch_ip, uplink_port):
 
 
 def get_edge_devices():
-    '''Функция заполняет список edge-свичей. Это нужно, чтобы потом
-    не опрашивать их, когда будем работать с транзитными девайсами.
-    И дополнительно они послужат нам опорой, когда будем строить линии от центра к edge'''
+    '''Build edge_devices dict for feature use'''
     global uplink_ip
     for i, device_ip in enumerate(devices.keys()):
         progress(i, 'Check edge status of device')
@@ -147,12 +146,12 @@ def get_edge_devices():
             if switch_is_edge(device_ip, devices[device_ip]['uplink']):
                 edge_devices.append(device_ip)
         else:
-            print 'Uplink not found on', device_ip
+            print('Uplink not found on ' + device_ip)
 
 
 def get_transit_devices(edge_device_mac):
-    '''В этой функции попытаемся найти все транзитные свичи. Это те свичи,
-        у которых в базе есть мак edge-свича на одном порту и мак аплинка на другом'''
+    '''We will find transit devices for edge switch.
+    Transit switch has uplink mac-address on uplink port and mac of edge switch on other port'''
     transit = []
     with sqlite3.connect(dbname) as con:
         con.row_factory = sqlite3.Row
@@ -174,8 +173,7 @@ def get_transit_devices(edge_device_mac):
 
 
 def get_port_by_mac(ip, mac):
-    '''Поучаем IP свича, на котором искать мак и отдаем порт,
-    на котором нашли этот мак. Так же определяем аплинк по мак-адресу агрегации'''
+    '''Get port where we see this mac on swith with this ip'''
     with sqlite3.connect(dbname) as con:
         con.row_factory = sqlite3.Row
         cursor = con.cursor()
@@ -191,81 +189,89 @@ def get_port_by_mac(ip, mac):
 
 
 def sort_transit_devices(transit_devices):
-    '''Эта функция пытается отсортировать список свичей так, чтобы в итоге получилась
-    линия, такая же как в реальной сети. Для каждого девайса из списка находим кол-во транзитных свичей.
-    Это кол-во и будет являтся индексом в готовом массиве. Т.е. для агрегации кол-во транзита = 0, значит он в
-    начале списка. И так далее'''
+    '''We will try to sort transit switches. Sorting is a process of creating a chain of switches
+    like in a real network. Last switch is edge. For every switch in transit list we calculate
+    their own transit list and check length of this list. The lenght will be index of the switch in
+    transit list. For example, uplink switch has no transit switches, his index is 0. 
+    Edge switch may have 3 transit switches, his index in result list in 3. And so on'''
+
     tmp_tr_list = [None] * len(transit_devices)
     for dev_ip in transit_devices:
         if 'transit' not in devices[dev_ip].keys():
-            # это нужно, чтобы вычислять транзит для одной железки только один раз
-            # одна железка в транзитах может встречаться несколько раз
+            # We save transit list in order to don't calculate transit switches twise for the switch
             devices[dev_ip]['transit'] = get_transit_devices(devices[dev_ip]['mac'])
         index = len(devices[dev_ip]['transit'])
         try:
             tmp_tr_list[index] = dev_ip
         except IndexError:
-            print 'Cant sort for dev_ip:', dev_ip, ', transit count (index):', index, 'tmp_tr_list len =', str(len(tmp_tr_list))
-            print 'Transit list on enter:', transit_devices, 'and searched mac', devices[dev_ip]['mac']
-            print 'Trying to continue calculation'
+            print('Cant sort for dev_ip: {dev_ip}, transit count (index): {index}, tmp_tr_list len = {tmp_tr_list_len}'.format(
+                tmp_tr_list_len=str(len(tmp_tr_list)),
+                dev_ip=dev_ip,
+                index=index
+            ))
+            print('Transit list on enter: {transit_devices}, and searched mac {mac}'.format(
+                mac=devices[dev_ip]['mac'],
+                transit_devices=str(transit_devices)
+            ))
+            print('Trying to continue calculation')
             continue
     return tmp_tr_list
 
 
 possible_uplink_mac = calc_uplink_mac()
-# эта переменная нужна, чтобы понять, где у свича аплинк
+# check uplink mac in config. If no mac, we will try to calculate it
 if 'uplink_mac' in cfg['network']:
     uplink_mac = mac.normalize_mac(cfg['network']['uplink_mac'])
-    print 'Using configured uplink mac', uplink_mac
+    print('Using configured uplink mac {mac}'.format(mac=uplink_mac))
 else:
     uplink_mac = possible_uplink_mac
-    print 'Configured uplink mac not found, using calculated mac', possible_uplink_mac
+    print('Configured uplink mac not found, using calculated mac {mac}'.formac(mac=possible_uplink_mac))
 
 uplink_ip = ''
 
 
-get_devices_list_from_db() # заполняем массив служебной информацией из таблицы ARP
-print
-get_set_uplinks() # заполняем в базе информацию у кого какой аплинк
-print
+get_devices_list_from_db()
+print()
+get_set_uplinks()
+print()
 
 
-# проверка для отдельного свича, если он передан в аргументе
+# If we specified --ip, we will calculate transit devices for this ip and exit
 if args.ip:
-    print 'Work with', args.ip
+    print('Work with ' + args.ip)
     ip_transit = get_transit_devices(edge_device_mac=devices[args.ip]['mac'])
-    print 'Transit:', ip_transit
+    print('Transit: ' + str(ip_transit))
     ip_tansit_sorted = sort_transit_devices(transit_devices=ip_transit)
-    print 'Sorted transit:', ip_tansit_sorted
+    print('Sorted transit: ' + str(ip_tansit_sorted))
     exit()
 
-get_edge_devices() # находим edge-девайсы. Потом мы пойдем по списке и будем искать промежуточные свичи
-print
+get_edge_devices()
+print()
 
-# Создаем файл для grapthviz. Можно проверить результат работы без zabbix
+# Creating grapthviz. This wil allow us to check result without zabbix
 dot = open(cfg['zabbix']['mapname'] + '.gv', 'w')
 dot.write('strict digraph switches {\n')
 
 
-# начальные значение координат в карте zabbix
+# Start position on zabbix-map
 x = 100
 y = 75
 
-added_hosts = [] # массив с hostid, которые уже были добавлены на карту, чтобы не добавлять повторно
-selements = [] # хранит хосты, которые будут добавлены на карту
-links = [] # хранит связи между узлами
-cnt = 1 # инкрементируется каждый раз, когда на карту добавляется хост. Сохраняется в виде id_on_map в массиве devices
-max_height = 480 # здесь будем хранить ширину карты. Чем больше хостов, тем шире. Это начальное значение
+added_hosts = [] # list with hostid already added to the map
+selements = [] # host will be added to the map
+links = [] # link between nodes
+cnt = 1 # host id count on map. Incrementing for every host added to the map
+max_height = 480 
+# 
 
 all_lists = []
 
 for counter, edge in enumerate(edge_devices, 1):
-    # получаем все транзитные девайсы
+    # get transit devices
     transit_list = get_transit_devices(edge_device_mac=devices[edge]['mac'])
-    # теперь нужно их отсортировать так, чтобы они шли в листе в том порядке,
-    # в котором подключены в сети
+    # sort transit devices
     sorted_transit_list = sort_transit_devices(transit_devices=transit_list)
-    # добавляем в начало узел агрегации, а в конец - узел edge
+    # insert uplink switch in the begining and edge at the end of list
     sorted_transit_list.insert(0, uplink_ip)
     sorted_transit_list.append(edge)
     all_lists.append(sorted_transit_list)
@@ -282,29 +288,24 @@ sorted_all_lists = []
 try:
     for elt, items in groupby(all_lists, itemgetter(min_transit_count)):
         for i in items:
-            # print(i)
             sorted_all_lists.append(i)
 except:
-    print 'Problem while sorting'
+    print('Problem while sorting')
     sorted_all_lists = all_lists.copy()
 
 # ==============================
 for counter, sorted_transit_list in enumerate(sorted_all_lists):
-    # Начальное значение для первго хоста по списку.
-    # Нужно, чтобы они не стояли рядом друго с другом, иначе надписи пересекаются
+    # Place host in chess order
     if counter % 2:
         y = 120
     else:
         y = 60
-    j = 1 # j - это "номер" строки, на которую добавляем хост. Чем дальше свич от агрегации, тем ниже он на карте
+    j = 1 # j - is the row number on the map. Edge device in the bottom of map. Uplink on the top
 
-    # после того, как линию построили, нужно сделать из нее строку 
-    # вида ip==ip==ip==ip, чтоб попарно добавить линии
-    # lnk_str = '=='.join(sorted_transit_list)
     if use_zabbix: 
         for dev in sorted_transit_list:
             j += 1
-            # этот хост еще не добавляли на карту
+            # this host is not yet added to the map
             if devices[dev]['hostid'] not in added_hosts:
                 selements.append({
                         'selementid': cnt,
@@ -319,11 +320,11 @@ for counter, sorted_transit_list in enumerate(sorted_all_lists):
                 y += 120    
                 devices[dev]['id_on_map'] = cnt
                 added_hosts.append(devices[dev]['hostid'])
-                cnt += 1 # selemetid Для каждого хоста будет уникален
+                cnt += 1 # selemetid is unique for every host
             y += 120    
         if max_height < y:
             max_height = y + 100
-        x += 100 # сдвигаемся правее
+        x += 100 # move to right
 
         # example: dev1 dev2 dev3 dev4 dev5
         for i in range(1, len(sorted_transit_list)):
@@ -357,5 +358,5 @@ if use_zabbix:
         mwidth=x+100, 
         mheight=max_height)
 else:
-    print 'Check map in', cfg['zabbix']['mapname'] + '.gv'
-print 'Done'
+    print('Check map in ' + cfg['zabbix']['mapname'] + '.gv')
+print('Done')
